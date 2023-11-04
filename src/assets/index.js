6f2d03df154bb6ccccb62c0be78ef450266246f4
Grenish rai <ariestheracer@gmail.com>
2023-11-04 21:49:13 +0530

    added background images

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import bbb from "./bbb.svg";
+import one from "./one.jpg";
+import two from "./two.jpg";
+import three from "./three.jpg";
+import four from "./four.jpg";
+import five from "./five.jpg";
+import six from "./six.jpg";
+import seven from "./seven.jpg";
+
+export { bbb, one, two, three, four, five, six, seven };
