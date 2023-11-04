64580a9112c8497bb71f9c09560fcc2d8af34047
Grenish rai <ariestheracer@gmail.com>
2023-11-04 22:55:53 +0530

    Update TextEditor.jsx

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import html2canvas from "html2canvas";
 
 const TextEditor = ({ selectedFont, selectedBackground }) => {
   const containerStyle = {
@@ -9,15 +10,37 @@ const TextEditor = ({ selectedFont, selectedBackground }) => {
     backgroundPosition: "center",
   };
 
+  const downloadTarget = () => {
+    const elem = document.getElementById("target");
+    if(!elem) {
+        return;
+    }
+    html2canvas(elem, {
+        scale: 5.5
+    }).then((canvas) => {
+
+        let image = canvas.toDataURL("image/jpeg");
+        const a = document.createElement("a");
+        a.href = image;
+        a.download = "QuotaShare_QuoteScreenShot.jpg";
+        a.click();
+
+    }).catch((err) => {
+        alert("Somethinf went wrong.")
+    })
+  }
+
   return (
     <div className="w-full h-screen overflow-y-scroll">
-      <div className="flex justify-center items-center w-[100%] h-screen">
+      <div className="flex flex-col justify-center items-center w-[100%] h-screen">
         <div
-          className="text-white w-1/2 p-5 border-[1px] rounded-lg outline-none min-h-[100px]"
+          className="text-white sm:w-1/2 w-11/12 p-5 border-[1px] rounded-lg outline-none min-h-[100px]"
           style={containerStyle}
+          id="target"
           contentEditable
-        >
-          Write something here...
+        ></div>
+        <div className="flex justify-end sm:w-1/2 w-11/12">
+          <button className="p-2 bg-green-300" onClick={downloadTarget}>Download</button>
         </div>
       </div>
     </div>
