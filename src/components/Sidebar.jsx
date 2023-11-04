1d7042a4bc81dbc13054cbe357cb87f2eb0cd195
Grenish rai <ariestheracer@gmail.com>
2023-11-04 22:55:50 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { one, two, three, four, five, six, seven } from "../assets";
 
 const Sidebar = ({ onSelectFont, onSelectBackground }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   const fonts = [
     {
       name: "Poppins",
@@ -40,41 +42,58 @@ const Sidebar = ({ onSelectFont, onSelectBackground }) => {
   };
 
   return (
-    <div className="bg-[#151516] p-6 h-screen overflow-y-scroll text-white flex-shrink-0">
-      <div className="flex flex-col gap-4 ">
-        <div className="font-bold text-lg mb-2">Font:</div>
-        {fonts.map((font) => (
-          <div
-            key={font.name}
-            onClick={() => handleFontClick(font.name)}
-            className="font-card bg-[#333333] rounded p-4 cursor-pointer hover:bg-[#3c3c3c] text-gray-200"
-            onKeyDown={(event) =>
-              event.key === "Enter" && handleFontClick(font.name)
-            }
-            role="button"
-            tabIndex="0"
-            aria-label={`Select ${font.name} font`}
-          >
-            <div>{font.name}</div>
-            <div className="text-xs mt-2" style={{ fontFamily: font.name }}>
-              {font.exampleText}
-            </div>
-          </div>
-        ))}
-        <div className="font-bold text-lg my-4">Background:</div>
-        {backgrounds.map((background, index) => (
+    <div className="bg-[#151516] sm:p-6 p-1 h-screen overflow-y-scroll text-white sm:flex flex-col">
+      <div className="flex flex-col gap-4 w-full">
+        {/* Hamburger menu button */}
+        <div className="block md:hidden">
           <button
-            key={index}
-            onClick={() => handleBackgroundClick(background)}
-            style={{
-              backgroundImage: `url(${background})`,
-            }} 
-            className="h-20 rounded cursor-pointer hover:opacity-80 text-white"
-            aria-label={`Select background ${index + 1}`}
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+            className="text-white p-2"
           >
-            Background {index + 1}
+            ☰
           </button>
-        ))}
+        </div>
+
+        {/* Sidebar content */}
+        <div
+          className={`${
+            isSidebarOpen ? "block" : "hidden md:block"
+          } md:flex flex-col gap-4 mx-auto md:mx-0 p-3`}
+        >
+          <div className="font-bold text-lg mb-2">Font:</div>
+          {fonts.map((font) => (
+            <div
+              key={font.name}
+              onClick={() => handleFontClick(font.name)}
+              className="font-card bg-[#333333] rounded p-4 cursor-pointer hover:bg-[#3c3c3c] text-gray-200"
+              onKeyDown={(event) =>
+                event.key === "Enter" && handleFontClick(font.name)
+              }
+              role="button"
+              tabIndex="0"
+              aria-label={`Select ${font.name} font`}
+            >
+              <div>{font.name}</div>
+              <div className="text-xs mt-2" style={{ fontFamily: font.name }}>
+                {font.exampleText}
+              </div>
+            </div>
+          ))}
+          <div className="font-bold text-lg my-4">Background:</div>
+          {backgrounds.map((background, index) => (
+            <button
+              key={index}
+              onClick={() => handleBackgroundClick(background)}
+              style={{
+                backgroundImage: `url(${background})`,
+              }}
+              className="h-20 w-full mb-2 rounded cursor-pointer hover:opacity-80 text-white"
+              aria-label={`Select background ${index + 1}`}
+            >
+              Background {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
